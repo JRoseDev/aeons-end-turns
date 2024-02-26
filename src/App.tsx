feba7b1ec5ef1d78f4a71fe2503db35da13b0df8
Joshua Rose <joshua.rose@squaredup.com>
2024-02-26 16:59:03 +0000

    Animate discarding the top card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,65 @@
 import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react';
 import { AECard, AECardType } from './components/AECard';
 import { Deck } from './components/Deck';
+import { motion } from 'framer-motion';
+import { useCallback, useRef, useState } from 'react';
 
 const makeCard = (type: AECardType) => {
     return <AECard type={type} isFaceUp={false} scale={2} />;
 };
 
 function App() {
+    const [deck, setDeck] = useState(
+        Array(50)
+            .fill(0)
+            .map(() => makeCard('Gem'))
+    );
+
+    const [discard, setDiscard] = useState(
+        Array(3)
+            .fill(0)
+            .map(() => <AECard type={'Spell'} isFaceUp={true} scale={2} />)
+    );
+
+    const deckRef = useRef<HTMLDivElement>(null);
+    const discardRef = useRef<HTMLDivElement>(null);
+
+    const discardTopCard = useCallback(() => {
+        if (deckRef.current == null || discardRef.current == null) {
+            return;
+        }
+
+        setDeck((d) => d.slice(0, d.length - 1));
+
+        /**
+         * Discard is the 'from' even though we're discarding from
+         * the deck to the discard because we're removing the old
+         * element from the deck before animating the new element
+         * we added to the discard.
+         */
+        const { x: fromX, y: fromY } =
+            discardRef.current.getBoundingClientRect();
+        const { x: toX, y: toY } = deckRef.current.getBoundingClientRect();
+
+        const start = { x: toX - fromX, y: toY - fromY };
+
+        const topCard = (
+            <motion.div
+                style={{
+                    transform: `translate(${start.x}px,${start.y}px)`,
+                }}
+                animate={{
+                    transform: 'translate(0,0)',
+                }}
+            >
+                {deck[deck.length - 1]}
+            </motion.div>
+        );
+        setDiscard((d) => d.concat(topCard));
+    }, [deck]);
+
     return (
-        <div className='h-screen w-full'>
+        <div className='w-full h-screen'>
             <Navbar position='sticky'>
                 <NavbarContent>
                     <NavbarBrand>Aeon's End Turns</NavbarBrand>
@@ -16,16 +67,14 @@ function App() {
             </Navbar>
             <div className='flex justify-between'>
                 <Deck
-                    cards={Array(50)
-                        .fill(0)
-                        .map(() => makeCard('Gem'))}
+                    ref={deckRef}
+                    cards={deck}
+                    onClick={() => {
+                        discardTopCard();
+                    }}
                 />
 
-                <Deck
-                    cards={Array(50)
-                        .fill(0)
-                        .map(() => makeCard('Gem'))}
-                />
+                <Deck ref={discardRef} cards={discard} />
             </div>
         </div>
     );
--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,9 +1,10 @@
-import { FC, ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 import clsx from 'clsx';
 
 interface DeckProps {
     className?: string;
     cards: ReactNode[];
+    onClick?: () => void;
 }
 
 function getRandom(min: number, max: number) {
@@ -12,19 +13,33 @@ function getRandom(min: number, max: number) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
 
-export const Deck: FC<DeckProps> = ({ cards, className }) => {
-    return (
-        <div className={clsx(className, 'relative p-2')}>
-            {cards.map((c, i) => {
-                return (
-                    <div
-                        className={clsx({ absolute: i > 0, 'top-0': i > 0 })}
-                        style={{ transform: `rotate(${getRandom(-2, 3)}deg)` }}
-                    >
-                        {c}
-                    </div>
-                );
-            })}
-        </div>
-    );
-};
+export const Deck = forwardRef<HTMLDivElement, DeckProps>(
+    ({ cards, className, onClick }, ref) => {
+        return (
+            <div
+                ref={ref}
+                className={clsx(className, 'relative p-2', {
+                    'cursor-pointer': onClick != null,
+                })}
+                onClick={() => onClick?.()}
+            >
+                {cards.map((c, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className={clsx({
+                                absolute: i > 0,
+                                'top-0': i > 0,
+                            })}
+                            style={{
+                                transform: `rotate(${getRandom(-2, 3)}deg)`,
+                            }}
+                        >
+                            {c}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+);
